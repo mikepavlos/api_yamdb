3c632045dfcaadd44b98dbdb805d5766f89ebd3f
Михаил Павлов <pavlovichmihaylovich@yandex.ru>
2022-11-29 00:45:10 +0300

    Create apps

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
