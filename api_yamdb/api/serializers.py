de6e4fd9d49ef3c7e6a67ab40d6d2372b4fb47c4
Чебенев Даниил <danya.chebenev@mail.ru>
2022-11-30 18:55:01 +0500

    Models_comp

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,1 +1,42 @@
 from rest_framework import serializers
+from reviews.models import Category, Genre, Title,
+
+
+class CategorySerializer(serializers.ModelSerializer):
+   
+    class Meta:
+        model = Category
+        fields = ('name', 'slug',)
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug',)
+        lookup_field = 'slug'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'rating',
+                  'description', 'genre', 'category')
+
+
+class TitleWriteSerializer(TitleSerializer):
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(), slug_field='slug'
+    )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(), slug_field='slug', many=True
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
