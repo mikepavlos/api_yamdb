a37829598e2374c849d911e2ad859f303f678de7
Михаил Павлов <pavlovichmihaylovich@yandex.ru>
2022-12-05 00:29:35 +0300

    Permissions: IsAdminRole, IsAdminOrReadOnly fix pytests

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,13 +1,21 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from reviews.models import User
 
 
-class IsAdminRole(permissions.BasePermission):
-    """Разрешения для администратора"""
+class IsAdminRole(BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
             and request.user.role == User.ADMIN
-            or request.user.is_staff
+            or request.user.is_superuser
+        )
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.role == User.ADMIN
             or request.user.is_superuser
         )
