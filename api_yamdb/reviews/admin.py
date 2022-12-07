2e9a8b3909539acaa357c51d75e5f396e7405cb3
Михаил Павлов <pavlovichmihaylovich@yandex.ru>
2022-12-07 22:55:10 +0300

    Admin settings

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Category, Comment, Genre, Review, Title, User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'bio',
+        'first_name',
+        'last_name'
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Genre)
+admin.site.register(Review)
+admin.site.register(Title)
