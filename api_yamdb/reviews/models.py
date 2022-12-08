eb24feecaa44a28b9d3d3fa8663de7534baecaf4
Михаил Павлов <pavlovichmihaylovich@yandex.ru>
2022-12-08 16:09:31 +0300

    Models/Common

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -43,28 +43,39 @@ class User(AbstractUser):
         return self.username
 
 
-class Category(models.Model):
+class CommonCategryGenre(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Category(CommonCategryGenre):
+    # name = models.CharField(max_length=256)
+    # slug = models.SlugField(unique=True, max_length=50)
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+class Genre(CommonCategryGenre):
+    # name = models.CharField(max_length=256)
+    # slug = models.SlugField(unique=True, max_length=50)
 
     class Meta:
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 
 class Title(models.Model):
